a3962d45ea91c5a808ce43a2d7e511cd7f5a0e2e
MakerM1 <132217491+MakerM1@users.noreply.github.com>
2023-11-01 22:45:02 +0400

    alert

--- scripts/index.js
+++ scripts/index.js
@@ -6,41 +6,49 @@ let turn = 'x'
 function determineWinner(sign) {
     if (boxes[0].innerHTML === `${sign}` && boxes[1].innerHTML === `${sign}` && boxes[2].innerHTML === `${sign}` ) {
         console.log(`${sign} win`)
+        alert(`${sign} won!`)
         boxes[0].style.color = 'green'
         boxes[1].style.color = 'green'
         boxes[2].style.color = 'green'
     } else if ( boxes[3].innerHTML === `${sign}` && boxes[4].innerHTML === `${sign}` && boxes[5].innerHTML === `${sign}` ) {
         console.log(`${sign} win`)
+        alert(`${sign} won!`)
         boxes[3].style.color = 'green'
         boxes[4].style.color = 'green'
         boxes[5].style.color = 'green'
     } else if (boxes[6].innerHTML === `${sign}` && boxes[7].innerHTML === `${sign}` && boxes[8].innerHTML === `${sign}`) {
         console.log(`${sign} win`)
+        alert(`${sign} won!`)
         boxes[6].style.color = 'green'
         boxes[7].style.color = 'green'
         boxes[8].style.color = 'green'
     } else if (boxes[0].innerHTML === `${sign}` && boxes[3].innerHTML === `${sign}` && boxes[6].innerHTML === `${sign}`) {
         console.log(`${sign} win`)
+        alert(`${sign} won!`)
         boxes[0].style.color = 'green'
         boxes[3].style.color = 'green'
         boxes[6].style.color = 'green'
     } else if (boxes[1].innerHTML === `${sign}` && boxes[4].innerHTML === `${sign}` && boxes[7].innerHTML === `${sign}`) {
         console.log(`${sign} win`)
+        alert(`${sign} won!`)
         boxes[1].style.color = 'green'
         boxes[4].style.color = 'green'
         boxes[7].style.color = 'green'
     } else if (boxes[2].innerHTML === `${sign}` && boxes[5].innerHTML === `${sign}` && boxes[8].innerHTML === `${sign}`) {
         console.log(`${sign} win`)
+        alert(`${sign} won!`)
         boxes[2].style.color = 'green'
         boxes[5].style.color = 'green'
         boxes[8].style.color = 'green'
     } else if (boxes[0].innerHTML === `${sign}` && boxes[4].innerHTML === `${sign}` && boxes[8].innerHTML === `${sign}`) {
         console.log(`${sign} win`)
+        alert(`${sign} won!`)
         boxes[0].style.color = 'green'
         boxes[4].style.color = 'green'
         boxes[8].style.color = 'green'
     } else if ( boxes[2].innerHTML === `${sign}` && boxes[4].innerHTML === `${sign}` && boxes[6].innerHTML === `${sign}`) {
         console.log(`${sign} win`)
+        alert(`${sign} won!`)
         boxes[2].style.color = 'green'
         boxes[4].style.color = 'green'
         boxes[6].style.color = 'green'
