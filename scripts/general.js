8e4009d49bfc6e44501ae1956d204f0ccd807161
MakerM1 <132217491+MakerM1@users.noreply.github.com>
2023-11-10 22:48:02 +0400

    added light theme v1

--- scripts/general.js
+++ scripts/general.js
@@ -1,5 +1,5 @@
 const warning = document.getElementById('warning')
-const overlay2 = document.querySelector('.overlay')
+const overlay2 = document.getElementById('warning-overlay')
 const closeButton = warning.querySelector('.close')
 
 function popUp() {
@@ -11,9 +11,27 @@ window.onload = popUp()
 
 overlay2.addEventListener('click', () => [
     warning.style.display = 'none'
+    ,overlay2.style.display = 'none'
 ])
 
 closeButton.addEventListener('click', () => {
     warning.style.display = 'none'
     overlay2.classList.remove('active')
 })
+
+const switchButton = document.querySelector(".js-theme");
+const circle = document.getElementById("circle");
+
+switchButton.addEventListener("click", () => {
+  circle.classList.toggle("dark");
+  console.log("dark");
+
+  if (circle.classList.contains("dark")) {
+    document.body.classList.toggle('light-theme');
+    circle.innerHTML = `<i class="fa-regular fa-sun"></i>`
+  } else {
+
+    document.body.classList.remove('light-theme')
+    circle.innerHTML = `<i class="fa-solid fa-moon"></i>`
+  }
+});
